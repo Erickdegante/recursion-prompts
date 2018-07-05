56b1167a1c201f82d48fd8e6685fea5c739c9c12
Erickdegante3003 <erickdegante3003@gmail.com>
2018-07-05 17:44:22 -0500

    problem 6 solved

--- src/recursion.js
+++ src/recursion.js
@@ -74,9 +74,16 @@ if(n === 0){
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
-};
+ // base case
+ if (y - x === 1 || y - x === 0) {
+   return [];
+ }
 
-// 7. Compute the exponent of a number.
+ // recursive case
+ y = y > x ? y - 1 : y + 1
+ return y === x ? [] : range(x,y).concat(y);
+};
+//}; 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
 // exponent(4,3); // 64
